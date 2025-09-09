1f64ed59127c4049d9def64be0c6b4e3c0d7e8c6
SAKSHI <sakshi.lambajhanji1@nhs.net>
2025-09-09 14:41:30 +0000

    add logic for eligibility

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,19 @@ const express = require('express')
 
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+// Called when answering question about whether NHS number is known
+router.post('/magical-powers-answer', (req, res) => {
 
+const hasSymptoms= req.session.data.hasSymptoms
+// Check whether the variable matches a condition 
+if (hasSymptoms==='Yes'){
+    res.redirect('/details')
+}
+else if (hasSymptoms==='No'){
+    res.redirect('/ineligible')
+}
+else {res.redirect('/magical-powers')
+
+} 
+});
 module.exports = router
